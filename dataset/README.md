c6b1536bd81e9729e15bfa8be8e09115fa16e878
w-chunping <danielwang20040527@gmail.com>
2025-12-07 00:33:47 +0800

    fixed static noise and add txt mode for pipeline

---
+++ dataset/README.md
@@ -0,0 +1,44 @@
+
+## usage
+
+Under project root:
+
+- common:
+  --config_path: path to your degradation pipeline config file
+  --make_video: will make video if raised
+
+- modes:
+  1. single: degrades a single video
+  
+    --input_root: the folder *directly* containing frames of the desired video to degrade
+    --output_root: the *parent* folder that wraps the folder containing the output pack
+
+    example:
+    ```
+    python dataset/degrade_video_albu.py --input_root ../dataset/GOT10/test/GOT-10k_Test_000001/ \ --output_root data/GOT10/test --make_video --mode single  --config_path dataset/aug_strong.yaml 
+    ```
+
+  2. multiple: degrades multiple videos
+
+    --input_root: the folder containing folders of videos that you want to degrade
+    --output_root: the parent folder that contains the folders of the result videos
+
+    example:
+    ```
+    python dataset/degrade_video_albu.py --input_root ../dataset/GOT10/test/ \
+    --output_root data/GOT10/test --mode multiple --config_path dataset/aug_strong.yaml
+    ```
+
+  3. txt: the videos are indicated in a txt file
+
+    --input_root: the folder that at least contains every folders of videos that you want to degrade
+    --output_root: the parent folder that contains the folders of the result videos
+    --txt: the path to the txt file which indicates the *relative path* from the input_root to the video
+
+    example:
+    ```
+    python dataset/degrade_video_albu.py --input_root ../dataset/GOT10/test/ \
+    --output_root data/GOT10/test --mode txt --txt dataset/test_subset.txt --config_path dataset/aug_strong.yaml
+    ```
+  
+
--- dataset/degrade_video_albu.py
+++ dataset/degrade_video_albu.py
@@ -7,6 +7,7 @@ import albumentations as A
 import argparse
 import shutil
 import yaml
+import copy
 
 # ===========================
 # Helper Functions
@@ -153,6 +154,41 @@ def degrade_single_sequence(seq_folder, input_root, output_root, cfg, make_vid =
     augmented = transform(images=video)
     degraded = augmented["images"]
 
+    # apply noise
+    if "noise" in cfg:
+        noise_cfg = cfg["noise"]
+        
+        # if noise_cfg is a list, take the first element
+        if isinstance(noise_cfg, list):
+            if len(noise_cfg) == 0:
+                return degraded
+            noise_cfg = noise_cfg[0]
+
+        noise_type = getattr(A, noise_cfg["type"])
+        noise_params = copy.deepcopy(noise_cfg.get("params", {}))
+
+       # deterministic but unique per sequence
+        # seq_seed = abs(hash(seq)) % (2**32)
+        # np.random.seed(seq_seed)
+        gn_mean = np.random.uniform(low = noise_params.get("mean_range", [0.0, 0.0])[0],
+                                    high = noise_params.get("mean_range", [0.0, 0.0])[1])
+        # print(f"[DEBUG] Sampled gaussian noise mean = {gn_mean} across the video.")
+        noise_params["mean_range"] = [gn_mean, gn_mean]
+
+        gn_std = np.random.uniform(low = noise_params.get("std_range", [0.2, 0.44])[0],
+                                   high = noise_params.get("std_range", [0.2, 0.44])[1])
+        # print(f"[DEBUG] Sampled gaussian noise std = {gn_std} across the video.")
+        noise_params["std_range"] = [gn_std, gn_std]
+
+        noise_p = noise_cfg.get("p", 1.0)
+        noise_transform = noise_type(p=noise_p, **noise_params)
+
+        # apply per-frame
+        degraded = [
+            noise_transform(image=frame)["image"]
+            for frame in degraded
+        ]
+
     # Save outputs
     save_frames(video, frame_files, original_dir)
     save_frames(degraded, frame_files, degraded_dir)
@@ -178,25 +214,62 @@ if __name__ == "__main__":
                         help="Output dataset root (will mirror structure)")
     parser.add_argument("--make_video", action="store_true")
     parser.add_argument("--config_path", default="dataset/default_aug.yaml", type = str)
+    parser.add_argument("--mode", required=True, type = str,
+                        help = "'single' for one vid, your input_root should be a directory directly containing frame images; \
+                            'multiple' for a folder containing multiple videos, your input_root should be a dirctory containing directories of frames; \
+                            'txt' mode requires a txt file as argument indicating the relative paths of desired directories to the input_root, the input_root should be a directory containing multiple folders of frames.")
+    parser.add_argument("--txt", type = str, default = None)
     args = parser.parse_args()
 
     input_root = args.input_root
     output_root = args.output_root
+    mode = args.mode
 
     cfg = load_config(args.config_path)
 
-    # Case 1: Direct sequence folder
-    if is_sequence_folder(input_root):
-        # print(f"[DEBUG] input_root = {input_root}, dirname = {os.path.dirname(input_root)}, output_root = {output_root}")
-        degrade_single_sequence(input_root, os.path.dirname(input_root), output_root, cfg, args.make_video)
-        exit()
+    full_path = []
+    root = None
 
-    # Case 2: Folder of many sequences
-    seqs = get_sequence_folders(input_root)
-    if len(seqs) == 0:
-        print("[ERROR] No sequences found.")
-        exit(1)
+    if mode == 'single':
+        full_path.append(input_root)
+        root = os.path.dirname(os.path.normpath(input_root))
+        # print(f"[DEBUG] full_path = {full_path}, root = {root}")
+    elif mode == 'multiple':
+        full_path = get_sequence_folders(input_root)
+        root = input_root
+        if len(full_path) == 0:
+            print("[ERROR] No sequences found.")
+            exit(1)
+        print(f"[INFO] Found {len(full_path)} sequences.")
+    elif mode == 'txt':
+        if args.txt is None:
+            print('[ERROR] You have to specify your txt file with txt mode.')
+            exit(1)
+        with open(args.txt, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    full_path.append(os.path.join(input_root, line))
+        print(f"[INFO] Found {len(full_path)} sequences.")
+        root = input_root
 
-    print(f"[INFO] Found {len(seqs)} sequences.")
-    for seq in seqs:
-        degrade_single_sequence(seq, input_root, output_root, cfg, args.make_video)
+    for seq in full_path:
+        # print(f"[DEBUG] seq = {seq}, root = {root}")
+        degrade_single_sequence(seq, root, output_root, cfg, args.make_video)
+    print("[INFO] Finished degrading videos.")
+
+    # # Case 1: Direct sequence folder
+    # if is_sequence_folder(input_root):
+    #     # print(f"[DEBUG] input_root = {input_root}, dirname = {os.path.dirname(input_root)}, output_root = {output_root}")
+    #     degrade_single_sequence(input_root, os.path.dirname(input_root), output_root, cfg, args.make_video)
+    #     exit()
+
+    # # Case 2: Folder of many sequences
+    # seqs = get_sequence_folders(input_root)
+    # if len(seqs) == 0:
+    #     print("[ERROR] No sequences found.")
+    #     exit(1)
+
+    # print(f"[INFO] Found {len(seqs)} sequences.")
+    # for seq in seqs:
+    #     degrade_single_sequence(seq, input_root, output_root, cfg, args.make_video)
